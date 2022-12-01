042a608ef83f6f7065893307e86a0f03a1aa470b
truongnx <truongmaytinh1997@gmail.com>
2022-12-01 09:27:44 +0700

    fix rate swap

--- programs/swap-token/src/utils/caculate.rs
+++ programs/swap-token/src/utils/caculate.rs
@@ -1,5 +1,5 @@
 pub fn caculate_token_sended(sol_payed: u64) -> u64 {
-    sol_payed.checked_mul(100).unwrap()
+    sol_payed.checked_mul(10).unwrap()
 }
 #[cfg(test)]
 mod tests {
@@ -8,6 +8,6 @@ mod tests {
 
     #[test]
     fn test_caculate_token_sended() {
-        assert_eq!(caculate_token_sended(1000000000), 100000000000);
+        assert_eq!(caculate_token_sended(1000000000), 10000000000);
     }
 }
